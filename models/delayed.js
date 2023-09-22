eb42bf3aa1ae79a510df0dea03cf5d3b24dfe52c
Melissa Johansson <mejo22@student.bth.se>
2023-09-22 16:37:29 +0200

    Eslint fixes

--- models/delayed.js
+++ models/delayed.js
@@ -1,17 +1,18 @@
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
 const delayed = {
     getDelayedTrains: function getDelayedTrains(req, res) {
         const query = `<REQUEST>
-                  <LOGIN authenticationkey="${process.env.TRAFIKVERKET_API_KEY}" />
-                  <QUERY objecttype="TrainAnnouncement" orderby='AdvertisedTimeAtLocation' schemaversion="1.8">
+                    <LOGIN authenticationkey="${process.env.TRAFIKVERKET_API_KEY}" />
+                    <QUERY objecttype="TrainAnnouncement" orderby='AdvertisedTimeAtLocation' 
+                    schemaversion="1.8">
                         <FILTER>
                         <AND>
                             <EQ name="ActivityType" value="Avgang" />
                             <GT name="EstimatedTimeAtLocation" value="$now" />
                             <AND>
                                 <GT name='AdvertisedTimeAtLocation' value='$dateadd(-00:15:00)' />
-                                <LT name='AdvertisedTimeAtLocation'                   value='$dateadd(02:00:00)' />
+                                <LT name='AdvertisedTimeAtLocation' value='$dateadd(02:00:00)' />
                             </AND>
                         </AND>
                         </FILTER>
@@ -27,23 +28,23 @@ const delayed = {
                         <INCLUDE>LocationSignature</INCLUDE>
                         <INCLUDE>TimeAtLocation</INCLUDE>
                         <INCLUDE>TrainOwner</INCLUDE>
-                  </QUERY>
+                    </QUERY>
             </REQUEST>`;
 
 
-            const response = fetch(
-                "https://api.trafikinfo.trafikverket.se/v2/data.json", {
-                    method: "POST",
-                    body: query,
-                    headers: { "Content-Type": "text/xml" }
-                }
-            ).then(function(response) {
-                return response.json()
-            }).then(function(result) {
-                return res.json({
-                    data: result.RESPONSE.RESULT[0].TrainAnnouncement
-                });
-            })
+        fetch(
+            "https://api.trafikinfo.trafikverket.se/v2/data.json", {
+                method: "POST",
+                body: query,
+                headers: { "Content-Type": "text/xml" }
+            }
+        ).then(function(response) {
+            return response.json();
+        }).then(function(result) {
+            return res.json({
+                data: result.RESPONSE.RESULT[0].TrainAnnouncement
+            });
+        });
     }
 };
 
