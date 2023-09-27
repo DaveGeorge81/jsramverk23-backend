7a1fc408440ab16dea87333d1236625fbdbc0b5b
Melissa Johansson <mejo22@student.bth.se>
2023-09-27 16:52:37 +0200

    Change from fetchTrainPositions to trains.fetchTrainPositions

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 // const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const fetchTrainPositions = require('./models/trains.js');
+const trains = require('./models/trains.js');
 const delayed = require('./routes/delayed.js');
 const tickets = require('./routes/tickets.js');
 const codes = require('./routes/codes.js');
@@ -44,6 +44,6 @@ const server = httpServer.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
 
-fetchTrainPositions(io);
+trains.fetchTrainPositions(io);
 
 module.exports = server;
